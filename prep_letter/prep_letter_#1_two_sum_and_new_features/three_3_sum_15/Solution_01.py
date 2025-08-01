9aedadb3ddc81d428bccf77df9eefa21b048c687
shuvomonowar00 <monowarhossain41998@gmail.com>
2025-08-02 03:44:02 +0600

    Solve LeetCode #15: 3Sum using two-pointer approach

--- prep_letter/prep_letter_#1_two_sum_and_new_features/three_3_sum_15/Solution_01.py
+++ prep_letter/prep_letter_#1_two_sum_and_new_features/three_3_sum_15/Solution_01.py
@@ -1,21 +1,29 @@
-'''
+"""
     15. 3Sum
-'''
+"""
 from typing import List
 
 
 class Solution_01:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        fnl_lst: List[int] = []
+        fnl_lst: List[List[int]] = []
 
         for i in range(len(nums)-2):
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
             left = i+1
             right = len(nums)-1
             while left < right:
                 if nums[i]+nums[left]+nums[right] == 0:
                     fnl_lst.append([nums[i], nums[left], nums[right]])
-                    left 
+                    # Skip the duplicates
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
                 elif nums[i]+nums[left]+nums[right] < 0:
                     left += 1
                 else:
@@ -23,8 +31,28 @@ class Solution_01:
         
         return fnl_lst
 
+# This solution has TLE problem :)
+# class Solution_01:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         nums.sort()
+#         fnl_lst: List[List[int]] = []
 
-if __name__ == '__main__':
+#         for i in range(len(nums)-2):
+#             left = i+1
+#             right = len(nums)-1
+#             while left < right:
+#                 if nums[i]+nums[left]+nums[right] == 0:
+#                     fnl_lst.append([nums[i], nums[left], nums[right]])
+#                     left += 1
+#                 elif nums[i]+nums[left]+nums[right] < 0:
+#                     left += 1
+#                 else:
+#                     right -= 1
+#         fnl_lst = [list(t) for t in {tuple(st) for st in fnl_lst}]
+#         return fnl_lst
+
+
+if __name__ == "__main__":
     obj = Solution_01()
     print(obj.threeSum([-1,0,1,2,-1,-4]))
         
