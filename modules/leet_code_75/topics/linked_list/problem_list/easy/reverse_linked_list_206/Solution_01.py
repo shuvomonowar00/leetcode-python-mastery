5cd5baac8ae1f8fd36dbcea3a793e61b5b69fc0a
shuvomonowar00 <monowarhossain41998@gmail.com>
2025-08-05 02:34:35 +0600

    Solve LeetCode #206: Reverse Linked List

--- modules/leet_code_75/topics/linked_list/problem_list/easy/reverse_linked_list_206/Solution_01.py
+++ modules/leet_code_75/topics/linked_list/problem_list/easy/reverse_linked_list_206/Solution_01.py
@@ -1,9 +1,14 @@
-'''
-
-'''
+"""
+    206. Reverse Linked List
+"""
 from typing import Optional
 
-
-class Solution:
+class Solution_01:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        tmp_tmp_node = None
+        while head != None:
+            tmp_node = head.next
+            head.next = tmp_tmp_node
+            tmp_tmp_node = head
+            head = tmp_node
+        return tmp_tmp_node
